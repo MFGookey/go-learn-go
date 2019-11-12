feff03ec81e8212c9c89f0f4298053a39c03f2e1
MFGookey <MFGookey@users.noreply.github.com>
2019-11-12 12:41:37 -0500

    Added errors when name or greeting are blank.

--- greeter/greeter.go
+++ greeter/greeter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -10,7 +11,12 @@ import (
 func main() {
 
 	name := getUsersName()
-	fmt.Println(Greet(name))
+	greeting, err := Greet(name)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(greeting)
+	}
 }
 
 func getUsersName() string {
@@ -22,11 +28,19 @@ func getUsersName() string {
 }
 
 //Greet Greet a person with Hello
-func Greet(name string) string {
+func Greet(name string) (string, error) {
 	return GreetWithGreeting("Hello", name)
 }
 
 //GreetWithGreeting Greet a person with a specific greeting
-func GreetWithGreeting(greeting string, name string) string {
-	return fmt.Sprintf("%s, %s", greeting, name)
+func GreetWithGreeting(greeting string, name string) (string, error) {
+	if name != "" && greeting != "" {
+		return fmt.Sprintf("%s, %s", greeting, name), nil
+	} else {
+		if name == "" {
+			return "", errors.New("name cannot be blank")
+		}
+
+		return "", errors.New("greeting cannot be blank")
+	}
 }
--- greeter/greeter_test.go
+++ greeter/greeter_test.go
@@ -6,50 +6,52 @@ import (
 )
 
 func Test_Greet_BlankName(t *testing.T) {
-	blankGreeting := "Hello, "
-	result := Greet("")
-	if result != blankGreeting {
-		t.Errorf("Blank name greeting was incorrect.  Expected \"%s\", but got \"%s\"", blankGreeting, result)
+	result, err := Greet("")
+	if err == nil {
+		t.Errorf("Blank name greeting was incorrect.  Expected error, but got \"%s\"", result)
 	}
 }
 
 func Test_Greet_WithName(t *testing.T) {
 	nameToGreet := "aslkjasdflkjh"
 	expectedResult := fmt.Sprintf("Hello, %s", nameToGreet)
-	result := Greet(nameToGreet)
+	result, err := Greet(nameToGreet)
 
 	if result != expectedResult {
 		t.Errorf("Greeting with name \"%s\" was incorrect.  Expected \"%s\" but got \"%s\"", nameToGreet, expectedResult, result)
+	}
+
+	if err != nil {
+		t.Errorf("Greeting with name \"%s\" was incorrect.  Expected \"%s\" but got error: %s", nameToGreet, expectedResult, err)
 	}
 }
 
 func Test_GreetWithGreeting_BlankName_BlankGreeting(t *testing.T) {
 	greetingToUse := ""
 	nameToUse := ""
-	expectedResult := fmt.Sprintf("%s, %s", greetingToUse, nameToUse)
-	result := GreetWithGreeting(greetingToUse, nameToUse)
-	if result != expectedResult {
-		t.Errorf("Greet with greeting with blank name and blank greeting was incorrect.  Expected \"%s\" but got \"%s\"", expectedResult, result)
+
+	result, err := GreetWithGreeting(greetingToUse, nameToUse)
+
+	if err == nil {
+		t.Errorf("Greet with greeting with blank name and blank greeting was incorrect.  Expected error but got \"%s\"", result)
 	}
 }
 
 func Test_GreetWithGreeting_Name_BlankGreeting(t *testing.T) {
 	greetingToUse := ""
 	nameToUse := "a;sdkjfafd;h"
-	expectedResult := fmt.Sprintf("%s, %s", greetingToUse, nameToUse)
-	result := GreetWithGreeting(greetingToUse, nameToUse)
-	if result != expectedResult {
-		t.Errorf("Greet with greeting with blank name and blank greeting was incorrect.  Expected \"%s\" but got \"%s\"", expectedResult, result)
+	result, err := GreetWithGreeting(greetingToUse, nameToUse)
+	if err == nil {
+		t.Errorf("Greet with greeting with blank name and blank greeting was incorrect.  Expected error but got \"%s\"", result)
 	}
 }
 
 func Test_GreetWithGreeting_BlankName_Greeting(t *testing.T) {
 	greetingToUse := "laisdufhalwiuef"
 	nameToUse := ""
-	expectedResult := fmt.Sprintf("%s, %s", greetingToUse, nameToUse)
-	result := GreetWithGreeting(greetingToUse, nameToUse)
-	if result != expectedResult {
-		t.Errorf("Greet with greeting with blank name and blank greeting was incorrect.  Expected \"%s\" but got \"%s\"", expectedResult, result)
+	result, err := GreetWithGreeting(greetingToUse, nameToUse)
+	if err == nil {
+		t.Errorf("Greet with greeting with blank name and blank greeting was incorrect.  Expected error but got \"%s\"", result)
 	}
 }
 
@@ -57,8 +59,13 @@ func Test_GreetWithGreeting_Name_Greeting(t *testing.T) {
 	greetingToUse := "asasfafs"
 	nameToUse := "ASFafsaFS"
 	expectedResult := fmt.Sprintf("%s, %s", greetingToUse, nameToUse)
-	result := GreetWithGreeting(greetingToUse, nameToUse)
+	result, err := GreetWithGreeting(greetingToUse, nameToUse)
+
+	if err != nil {
+		t.Errorf("Greet with greeting with name and greeting was incorrect.  Expected \"%s\" but got error \"%s\"", expectedResult, err)
+	}
+
 	if result != expectedResult {
-		t.Errorf("Greet with greeting with blank name and blank greeting was incorrect.  Expected \"%s\" but got \"%s\"", expectedResult, result)
+		t.Errorf("Greet with greeting with name and greeting was incorrect.  Expected \"%s\" but got \"%s\"", expectedResult, result)
 	}
 }
